02e520869769db29d52f8e57359d8253fdbef991
fimar <fimar@MARIOV>
2018-03-15 22:31:45 -0300

    actualización de variables

---
+++ test/Clase01.java
@@ -0,0 +1,21 @@
+
+public class Clase01 {
+    
+     public static void main(String[] args) {
+              
+        
+        System.out.println("Franco apellido INS121");
+        /*
+        instalar JAVA 1.8
+        corroborar instalación mediante el comando "java -version"
+        configurar variable de entorno de java "JAVA_HOME"     
+        crear clase que imprime sus nombre en archivo de texto
+            compilar la clase java creada en un archivo de texto, cuya extensión cambiarán a java 
+                ejemplo (clase1.txt --> clase1.java
+                abrir consola de comandos
+                ejecutar el utilitario javac
+        
+        */
+    }
+    
+}
